b59fe29db4e0987a9e3253c0ca66b292e4981785
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-03-26 16:51:38 +0900

    スプラッシュ画面追加

--- app/src/main/java/com/example/android_splashscreen/Splash.java
+++ app/src/main/java/com/example/android_splashscreen/Splash.java
@@ -2,13 +2,34 @@ package com.example.android_splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // 2秒したらMainActivityを呼び出してSplashActivityを終了する
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                // MainActivityを呼び出す
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                // アクティビティ(MainActivity)を起動する
+                startActivity(intent);
+                // SplashActivityを終了する
+                Splash.this.finish();
+            }
+        }, 2 * 1000); // 2000ミリ秒後（2秒後）に実行
     }
 }
